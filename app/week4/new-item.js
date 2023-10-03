d021263fc99c4dd80f7db6230347305e5ea4d896
stephaniemartyna <stephanie.martyna@edu.sait.ca>
2023-10-03 10:31:31 -0600

    Sizing fix

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -34,8 +34,8 @@ export default function NewItem() {
     
         return (
             <main className="flex justify-center">
-                <div className=" mt-4 w-full max-w-md bg-white p-8 rounded-lg shadow-md ">
-                  <h1 className="pl-2 text-2xl text-zinc-800 font-bold mb-8">
+                <div className=" mt-4 w-85 max-w-md bg-white p-8 rounded-lg shadow-md ">
+                  <h1 className="pl-2 text-xl text-zinc-800 font-bold mb-4">
                     Add Item To Shopping List:
                   </h1>
                   <form onSubmit={handleSubmit}>
@@ -47,7 +47,7 @@ export default function NewItem() {
                         placeholder="Item Name"
                         onChange={handleNameChange}
                         value={name}
-                        className="ml-1 p-2 block w-full rounded-md placeholder-zinc-400 text-black bg-gray-100 focus:bg-white"
+                        className="text-sm ml-1 p-1 block w-full rounded-md placeholder-zinc-400 text-black bg-gray-100 focus:bg-white"
                       />
                     </label>
                     </div>
@@ -58,7 +58,7 @@ export default function NewItem() {
                         required
                         onChange={handleQuantityChange}
                         value={quantity}
-                        className="ml-1 w-20 p-2 rounded-md text-black bg-zinc-100 focus:bg-white"
+                        className="text-sm ml-1 w-20 p-1 rounded-md text-black bg-zinc-100 focus:bg-white"
                       />
                     </label>
                     <label className="block mb-4 ">
@@ -67,7 +67,7 @@ export default function NewItem() {
                         name="category"
                         value={category}
                         onChange={handleCategoryChange}
-                        className="ml-1 p-2 w-full rounded-md text-black bg-zinc-100 focus:bg-white ">
+                        className="text-sm ml-1 p-1 w-full rounded-md text-black bg-zinc-100 focus:bg-white ">
                         <option value disabled> Category</option>
                         <option value="Produce">Produce</option>
                         <option value="Dairy">Dairy</option>
@@ -86,10 +86,10 @@ export default function NewItem() {
                     <button
                       type="submit"
                       onSubmit={handleSubmit}
-                      className="ml-1 p-2 block w-full bg-fuchsia-300 hover:bg-fuchsia-500 rounded-md text-white">
+                      className="ml-1 p-1 block w-full bg-fuchsia-300 hover:bg-fuchsia-500 rounded-md text-white">
                       +
                     </button>
-                    <div className='text-center italic text-xs mt-3 text-zinc-400'>
+                    <div className='text-center italic text-xs mt-4 text-zinc-400'>
                     <Link href="/">Go to Home Page</Link> 
                     </div>
                   </form>
